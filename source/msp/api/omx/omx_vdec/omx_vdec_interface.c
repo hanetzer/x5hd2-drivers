65fed5c6430f993770a90d5114ec89c34484ad3b
caps.liu <caps.liu@foxmail.com>
2014-08-01 12:02:06 +0800

    add omx vdec mpi interface and modify Makefile 	modified:   source/msp/api/omx/omx_vdec/Makefile 	new file:   source/msp/api/omx/omx_vdec/omx_vdec_interface.c 	Signed-off-by caps.liu@foxmail.com

---
+++ source/msp/api/omx/omx_vdec/omx_vdec_interface.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C), 2001-2011, Hisilicon Tech. Co., Ltd.
+ * File Description: omx mpi interface
+ */
+ 
+ #include "hi_mpi_omx.h"
+ 
+ HI_S32 
+ HI_MPI_OMXVdec_GetRealseFrameInterface(OMXVDEC_INTERFACE_S *omxvdec_interface)
+ {
+    return 0;
+ }
+ 
